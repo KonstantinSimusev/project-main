363eb18813f91d220fd8f6b9124e9d55a30efb42
Konstantin Simusev <simussev.k@icloud.com>
2024-07-28 23:52:59 +0500

    bugfix_2.07: src

--- src/components/grups.js
+++ src/components/grups.js
@@ -50,395 +50,395 @@ export const grups = [
       },
     ]
   },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: '',
-  //   answers: [
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //     {
-  //       title: '',
-  //       bool: false
-  //     },
-  //   ]
-  // },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
+  {
+    title: '',
+    answers: [
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+      {
+        title: '',
+        bool: false
+      },
+    ]
+  },
 ]
