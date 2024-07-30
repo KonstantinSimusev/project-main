dd35af48bf87d973195b5f6f89da9690c2d82ed1
Konstantin Simusev <simussev.k@icloud.com>
2024-07-31 00:14:39 +0500

    bugfix_2.20: src

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,8 @@ import { grups } from './data/grups';
 import { technology } from './data/technology';
 import { management } from './data/management';
 import { smk } from './data/smk';
+import { ot } from './data/ot';
+
 import { createQuestion } from './card';
 
 const questionsList = document.querySelector('.questions__list');
@@ -13,6 +15,7 @@ const grupsButton = document.querySelector('.button__grups');
 const technologyButton = document.querySelector('.button__technology');
 const managementButton = document.querySelector('.button__management');
 const smkButton = document.querySelector('.button__smk');
+const otButton = document.querySelector('.button__ot');
 
 const buttons = document.querySelector('.buttons');
 const cleaningSection = document.querySelector('.cleaning');
@@ -55,6 +58,7 @@ createQuestions(grupsButton, grups);
 createQuestions(technologyButton, technology);
 createQuestions(managementButton, management);
 createQuestions(smkButton, smk);
+createQuestions(otButton, ot);
 
 clearButton.addEventListener('click', () => {
   inputs.forEach(input => {
