9d8af3999ea7bae7e8248076257d5ead4da4d479
Konstantin Simusev <simussev.k@icloud.com>
2024-07-31 09:26:55 +0500

    bugfix_2.22: src

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import '../pages/index.css';
+import { all } from './data/all';
 import { kodeks } from './data/kodeks';
 import { grups } from './data/grups';
 import { technology } from './data/technology';
@@ -9,11 +10,14 @@ import { tkrf } from './data/tkrf';
 import { production } from './data/production';
 import { machine } from './data/machine';
 import { electro } from './data/electro';
+import { lpc_5 } from './data/lpc_5'
+import { lpc_11 } from './data/lpc_11'
 
 import { createQuestion } from './card';
 
 const questionsList = document.querySelector('.questions__list');
 
+const allButton = document.querySelector('.button__all');
 const kodeksButton = document.querySelector('.button__kodeks');
 const grupsButton = document.querySelector('.button__grups');
 const technologyButton = document.querySelector('.button__technology');
@@ -24,6 +28,8 @@ const tkrfButton = document.querySelector('.button__tkrf');
 const productionButton = document.querySelector('.button__production');
 const machineButton = document.querySelector('.button__machine');
 const electroButton = document.querySelector('.button__electro');
+const lpc5Button = document.querySelector('.button__lpc_5');
+const lpc11Button = document.querySelector('.button__lpc_11');
 
 const buttons = document.querySelector('.buttons');
 const cleaningSection = document.querySelector('.cleaning');
@@ -61,6 +67,7 @@ const createQuestions = (button, questions) => {
   });
 }
 
+createQuestions(allButton, all);
 createQuestions(kodeksButton, kodeks);
 createQuestions(grupsButton, grups);
 createQuestions(technologyButton, technology);
@@ -71,6 +78,8 @@ createQuestions(tkrfButton, tkrf);
 createQuestions(productionButton, production);
 createQuestions(machineButton, machine);
 createQuestions(electroButton, electro);
+createQuestions(lpc5Button, lpc_5);
+createQuestions(lpc11Button, lpc_11);
 
 clearButton.addEventListener('click', () => {
   inputs.forEach(input => {
